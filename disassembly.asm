064e2e2ba489dc77f48568696e3c1368cca76401
Miguel Colom <miguel@mcolom.info>
2024-11-26 18:58:23 +0100

    ed

--- disassembly.asm
+++ disassembly.asm
@@ -131,8 +131,17 @@ BALL_TABLE_IDX_ACTIVE: equ 0
 ; 2: ball moves normally
 BALL_TABLE_IDX_GLUE: equ 1
 ;
-BALL_TABLE_IDX_SPEED_COUNTER: equ 13
+; 01, 02: ball going down
+; FF, FE: ball going up
+;BALL_TABLE_: equ 2
+
+;BALL_TABLE_: equ 3
+; 01, 02: ball going right
+; FF, FE: ball going left
+
+;
 BALL_TABLE_IDX_SPEED_POS: equ 7
+BALL_TABLE_IDX_SPEED_COUNTER: equ 13
 BALL_TABLE_IDX_GLUE_COUNTER: equ 14
 
 TABLE_UNKNOWN_1: equ 0xe101
